792793b964314d9f6430ff510a25c34f94c05511
Ayocode5 <81022162+Ayocode5@users.noreply.github.com>
2021-05-03 17:45:34 +0700

    Update ui.blade.php

--- vendor/austintoddj/canvas/resources/views/ui.blade.php
+++ vendor/austintoddj/canvas/resources/views/ui.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Canvas</title>
+    <title>ayocode</title>
 
     <link rel="stylesheet" type="text/css" href="{{ mix('css/canvas-ui.css') }}">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
